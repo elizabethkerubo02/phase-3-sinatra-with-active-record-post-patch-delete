fa56437a9eaf70d0edfaec0b559a30fefd6a2d60
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-06-07 14:13:56 +0300

    done

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ ENV['RACK_ENV'] ||= "development"
 # Require in Gems
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
+use Rack::JSONBodyParser
 
 # Require in all files in 'app' directory
 require_all 'app'
